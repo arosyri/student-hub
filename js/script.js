f0a458d48f974175a2d0bcd48065191ebd4e37ec
arosyri <karina.korchova@gmailcom>
2026-04-30 22:13:35 +0900

    add: реалізація бургер-меню та інтерактивності

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById("burger");
+const navList = document.getElementById("navList");
+
+burger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+});
